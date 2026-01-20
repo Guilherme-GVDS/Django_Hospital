7295d0b43562413fb2ca50a7dd2c07c971a36cb7
Guilherme-GVDS <vini.guilherme1800@gmail.com>
2026-01-20 15:16:59 -0300

    feat: Paciente

---
+++ paciente/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from datetime import date
+# Create your models here.
+class Paciente(models.Model):
+
+    SEXO_CHOICES=[
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='paciente')
+    foto= models.ImageField(upload_to='foto', blank=True, null=True)
+    cpf = models.CharField(max_length=11, unique=True,
+        validators=[RegexValidator(r'^\d{11}$', 'CPF deve conter 11 dígitos')]
+    )
+    data_nascimento = models.DateField()
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    celular = models.CharField(
+        max_length=11,
+        validators=[RegexValidator(r'^\d{10,11}$', 'Celular deve conter 10 ou 11 dígitos')]
+    )
+    cep = models.CharField(
+        max_length=8,
+        validators=[RegexValidator(r'^\d{8}$', 'CEP deve conter 8 dígitos')]
+    )
+    numero = models.CharField(max_length=10)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
+        ordering = ['-data_cadastro']
+    
+    def __str__(self):
+        return f"{self.user.get_full_name()} - {self.cpf}"
+    
+    @property
+    def nome_completo(self):
+        return self.user.get_full_name()
+
+    @property
+    def email(self):
+        return self.user.email
+    
+    @property
+    def idade(self):
+        hoje = date.today()
+        return hoje.year - self.data_nascimento.year - (
+            (hoje.month, hoje.day) < (self.data_nascimento.month, self.data_nascimento.day)
+        )
